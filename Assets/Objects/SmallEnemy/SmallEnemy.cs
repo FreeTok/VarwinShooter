02b432be16ddea3da6475431694b4a8d5afdc6ef
Konstantin <freetokdevelop@gmail.com>
2021-08-13 22:32:55 +0300

    For duty

---
+++ Assets/Objects/SmallEnemy/SmallEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using WeaponLibrary;
+using UnityEngine;
+using Varwin;
+using Varwin.Public;
+
+namespace Varwin.Types.Enemy_fcb68f39c4314653b1448037ca5555b7
+{
+    [VarwinComponent(English: "Enemy")]
+    public class SmallEnemy : VarwinObject
+    {
+        // public enum EnEnemyClass
+        // {
+        //     Wizard,
+        //     Knight,
+        // };
+        //
+        // public EnEnemyClass EnemyClass;
+        //
+        // public GameObject WizardMesh, KnightMesh;
+        //
+        // void Start()
+        // {
+        //     switch(EnemyClass)
+        //     {
+        //         case EnEnemyClass.Wizard:
+        //         {
+        //             WizardMesh.SetActive(true);
+        //             print("Wizard");
+        //             break;
+        //         }
+        //
+        //         case EnEnemyClass.Knight:
+        //         {
+        //             KnightMesh.SetActive(true);
+        //             print("Knight");
+        //             break;
+        //         }
+        //     }
+        // }
+    }
+}
