d4b582846a7084570b67954395b0535b5859169a
Konstantin <freetokdevelop@gmail.com>
2021-08-16 11:35:29 +0300

    Added BigEnemy

---
+++ Assets/Objects/BigEnemy/BigEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Varwin;
+using Varwin.Public;
+
+namespace Varwin.Types.BigEnemy_6cc896cffcff4e8d95a8367963486dd1
+{
+    [VarwinComponent(English: "Big Enemy")]
+    public class BigEnemy : VarwinObject
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+             
+        }
+    }
+}
