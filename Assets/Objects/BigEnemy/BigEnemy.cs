b78005fa842c139b9ecbfefb72452524e48d9286
Konstantin <freetokdevelop@gmail.com>
2021-08-17 00:26:58 +0300

    For duty

--- Assets/Objects/BigEnemy/BigEnemy.cs
+++ Assets/Objects/BigEnemy/BigEnemy.cs
@@ -7,14 +7,40 @@ namespace Varwin.Types.BigEnemy_6cc896cffcff4e8d95a8367963486dd1
     [VarwinComponent(English: "Big Enemy")]
     public class BigEnemy : VarwinObject
     {
-        private void Start()
-        {
-            
-        }
+        private Vector3 _startPosition;
 
-        private void Update()
+        [Variable(English: "StartPosition", Russian: "Стартовая позиция")]
+        public Vector3 StartPositionPanel
         {
-             
+            get => _startPosition;
+            set => _startPosition = value;
+        }
+        
+        private float _GetX;
+
+        [Variable(English: "GetX", Russian: "Получить X")]
+        public float GetXPanel
+        {
+            get => _GetX;
+            set => _GetX = value;
+        }
+        
+        private float _GetY;
+
+        [Variable(English: "GetY", Russian: "Получить Y")]
+        public float GetYPanel
+        {
+            get => _GetY;
+            set => _GetY = value;
+        }
+        
+        private float _GetZ;
+
+        [Variable(English: "GetZ", Russian: "Получить Z")]
+        public float GetZPanel
+        {
+            get => _GetZ;
+            set => _GetZ = value;
         }
     }
 }
--- Assets/Objects/SmallEnemy/SmallEnemy.cs
+++ Assets/Objects/SmallEnemy/SmallEnemy.cs
@@ -7,6 +7,42 @@ namespace Varwin.Types.SmallEnemy_211023e0a08b4fe69ae12e0d1c19edba
     [VarwinComponent(English: "Small Enemy")]
     public class SmallEnemy : VarwinObject
     {
+        private Vector3 _startPosition;
+
+        [Variable(English: "StartPosition", Russian: "Стартовая позиция")]
+        public Vector3 StartPositionPanel
+        {
+            get => _startPosition;
+            set => _startPosition = value;
+        }
+        
+        private float _GetX;
+
+        [Variable(English: "GetX", Russian: "Получить X")]
+        public float GetXPanel
+        {
+            get => _GetX;
+            set => _GetX = value;
+        }
+        
+        private float _GetY;
+
+        [Variable(English: "GetY", Russian: "Получить Y")]
+        public float GetYPanel
+        {
+            get => _GetY;
+            set => _GetY = value;
+        }
+        
+        private float _GetZ;
+
+        [Variable(English: "GetZ", Russian: "Получить Z")]
+        public float GetZPanel
+        {
+            get => _GetZ;
+            set => _GetZ = value;
+        }
+        
         public enum EnEnemyClass
         {
             Wizard,
