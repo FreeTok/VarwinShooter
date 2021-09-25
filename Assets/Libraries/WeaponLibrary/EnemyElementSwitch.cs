92b707a42fcfbe7b16f7ced7abe47b3b5c7f87e6
Konstantin <freetokdevelop@gmail.com>
2021-09-25 22:51:46 +0300

    Actions replaces from libraries to main class

--- Assets/Libraries/WeaponLibrary/EnemyElementSwitch.cs
+++ Assets/Libraries/WeaponLibrary/EnemyElementSwitch.cs
@@ -5,26 +5,18 @@ using Varwin;
 
 public class EnemyElementSwitch : MonoBehaviour
 {
-    private Element.EnElements EnemyClass;
+    public Element.EnElements EnemyClass;
 
     public GameObject MeshHolder;
     private MeshRenderer[] Meshs;
-
-    [VarwinInspector(English: "Enemy class", Russian: "Класс врага")]
-    [Variable(English: "Enemy class")]
-    public Element.EnElements EnemyClassPanel
-    {
-        get => EnemyClass;
-    }
-
+    
     public Material DendroMaterial, IceMaterial, LightMaterial, DarknessMaterial;
 
     void Start()
     {
         Meshs = MeshHolder.GetComponentsInChildren<MeshRenderer>();
     }
-
-    [Action(English: "Switch enemy class")]
+    
     public void SwitchEnemyClass()
     {
         switch (EnemyClass)
@@ -58,8 +50,7 @@ public class EnemyElementSwitch : MonoBehaviour
             }
         }
     }
-
-    [Action(English: "Set random enemy class")]
+    
     public void RandomEnemyClass()
     {
         EnemyClass = (Element.EnElements)Random.Range(0, 3);
--- Assets/Objects/SmallEnemy/SmallEnemy.cs
+++ Assets/Objects/SmallEnemy/SmallEnemy.cs
@@ -7,6 +7,23 @@ namespace Varwin.Types.SmallEnemy_211023e0a08b4fe69ae12e0d1c19edba
     [VarwinComponent(English: "Small Enemy")]
     public class SmallEnemy : VarwinObject
     {
-        
+        [VarwinInspector(English: "Enemy class", Russian: "Класс врага")]
+        [Variable(English: "Enemy class")]
+        public Element.EnElements EnemyClassPanel
+        {
+            get => GetComponent<EnemyElementSwitch>().EnemyClass;
+        }
+
+        [Action(English: "Switch enemy class")]
+        public void SwitchEnemyClass()
+        {
+            GetComponent<EnemyElementSwitch>().SwitchEnemyClass();
+        }
+
+        [Action(English: "Set random enemy class")]
+        public void RandomEnemyClass()
+        {
+            GetComponent<EnemyElementSwitch>().RandomEnemyClass();
+        }
     }
 }
